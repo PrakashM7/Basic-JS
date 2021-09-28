61d18dd2b4f3885ba2e1542044d23b8c8d2439e7
PrakashM7 <mariyappanprakash17@gmail.com>
2021-09-28 21:28:22 +0530

    BMI

---
+++ codechallenge.js
@@ -0,0 +1,19 @@
+let mark , john;
+var mass , height,mass1 , height1 ;
+let BMI ,BMI1;
+mass =78;
+mass1 =92;
+height=1.69
+height1=1.95
+BMI = (mass /(height *height )) ;
+BMI1 = (mass1 /(height1 *height1 )) ;
+
+
+console.log('mark' ,BMI,"john", BMI1);
+if (BMI>BMI1)
+{
+console.log("mark", BMI );
+}
+else{
+    console.log("john",BMI1);
+}
