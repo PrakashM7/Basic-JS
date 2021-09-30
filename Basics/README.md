c38a5bf95baa4fa4358f78f721dd1cee9f9a25a5
PrakashM7 <91495074+PrakashM7@users.noreply.github.com>
2021-09-30 12:02:55 +0530

    Basics

---
+++ Basics/README.md
@@ -0,0 +1,5 @@
+#Basic-JS -27.09.21
+#code challenge BMI calculation (28.09.21) 
+#template literals most commanly used E6 feature  & string multiple lines (29.09.21)
+#code challenge
+
---
+++ Basics/script.js
@@ -0,0 +1,34 @@
+
+let js = "coder"; //variable declaration 
+
+    console.log(40 + 8 + 23 - 10); //operators 
+
+
+let firstName = "Prakash "; //initialize value to the variable
+
+console.log(firstName); //displayig that variable data 
+js ="java script"; //reinitialize value to the variable
+
+                    //let muttable  variable
+console.log(js);
+
+let $function = 27; //$f.. can use as a variable name
+console.log($function);
+
+let kite = true ; //true boolean
+console.log(kite);
+console.log(typeof kite); //type of the variable
+
+
+let age = 23;
+console.log(firstName +'age ' + age) // conc the values 
+
+// Assignment operators
+let x = 10 + 5; // 15 
+x += 10; // x = x + 10 = 25
+x *= 4; // x = x * 4 = 100 
+x++; // x = x + 1
+x--;
+x--;
+console.log(x);
+
