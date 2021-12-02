782e7a0b9658dffa96eb5fab4dcdcda8482bba1f
PrakashM7 <mariyappanprakash17@gmail.com>
2021-12-02 18:52:33 +0530

    array

--- fundamentals2.js
+++ fundamentals2.js
@@ -1,9 +1,8 @@
 //activate strict mode keyword 'use strict' in begining of the code (US is secure the code) 
 //create visible errors 
+ 'Use strict'
 
-/* 'Use strict'
-
- let hasdriverlicense =false;
+ /*let hasdriverlicense =false;
  const testpass= true;
 
  if(testpass) has.
@@ -29,3 +28,45 @@ driverslicense= true;
  console.log(applejuice);
  const orangejuice= fruit(42,10); //calling the function with different values 
  console.log(orangejuice);//return value stored in a variable orange juice 
+
+ //if else with function
+  /*const applelovers= prompt('are u a applelover? true or false');
+  console.log(applelovers);*/
+  const applelovers= true;
+  const orangelovers= true;
+ //const orangelovers= prompt('are u a orange lover? true or false');
+
+
+if(applelovers && orangejuice)
+{
+    const juiceorder = fruit(30,30); //assign value to function and store the return value in a constant
+    console.log('appple & orange are eqaul',juiceorder);  
+}
+//else if(applelovers===false && orangejuice ===true)
+else
+{
+    const juiceorder= fruit(10,50); //calling the function with different values 
+    console.log('orange juice greater than apple juice order list',juiceorder);//return value stored in a variable orange juice 
+
+}
+
+/*else
+{
+    const juiceorder= fruit(50,10); //calling the function with different values 
+    console.log('apple juice greater than  orange juice order list',juiceorder);//return value stored in a variable orange juice 
+
+}*/
+
+//arrays 
+
+//array declaration
+const friendsarray =['ajith', 'prakash', 'mahendran','roopan'];
+
+console.log (friendsarray);
+console.log (friendsarray[1]); //array 1st index value 
+console.log (friendsarray [friendsarray.length-1]);// last element present inarray n-1
+
+friendsarray[2] = 'gamer mahe';// you can replace one element in an array 
+console.log(friendsarray);
+//friendsarray = ['ajith','prakash', 'navanee','roopan']; //but u cant replace an whole array or multiple array in same line 
+//console.log(friendsarray);
