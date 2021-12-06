5ba2a643feddb4b7ea10f11d4d1b0ee1903aa48b
PrakashM7 <mariyappanprakash17@gmail.com>
2021-12-06 13:12:18 +0530

    array, methods

--- fundamentals2.js
+++ fundamentals2.js
@@ -70,3 +70,34 @@ friendsarray[2] = 'gamer mahe';// you can replace one element in an array
 console.log(friendsarray);
 //friendsarray = ['ajith','prakash', 'navanee','roopan']; //but u cant replace an whole array or multiple array in same line 
 //console.log(friendsarray);
+
+
+//array methods 
+
+const friendsarray1 =['ajith', 'prakash', 'mahendran','roopan'];
+//Add element prefix and suffix 
+
+//friendsarray1.push('surya'); //push is the function that add the data next to the last index in an present array 
+friendsarray1.unshift('master');// unshift function add data in begining of an array and it return length of an array 
+const newLength =friendsarray1.push('surya'); //push function does return the value that is length of new array
+console.log(friendsarray1);
+console.log(newLength); //length of an array stored in a variable newlength 
+
+
+//Remove element in array 
+const popped =friendsarray1.pop(); //last element  
+//pop return the removed element 
+console.log(popped);
+console.log(friendsarray1);
+
+friendsarray1.shift(); //remove first  element 
+console.log(friendsarray1.indexOf('prakash')); //return the index of an element 
+//if given element is not present in an array it will return -1
+//es6 method 
+console.log(friendsarray1.includes('prakash')); //return true or false 
+friendsarray1.push(23);
+console.log(friendsarray1.includes('23')); //it will return false because it is strict operator it does not do type cohersion 
+
+if (friendsarray1.includes('ajith'))   {
+    console.log('you have a friend ajith');
+}
